b6b8236b3ad0b0227ffcfad37074b4f0c90d5e12
MHA2001 <mha.akhoudndi@gmail.com>
2025-11-12 20:53:17 +0330

    refactor:  having dynamic Lists in sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,26 +1,37 @@
+"use client";
+
+import { useAppDispatch, useAppSelector } from "../store/Provider";
+import { selectAllLists, selectList, selectSelectedListId } from "../store/listSlice";
+import { List } from "@/src/types/list";
+
 export default function Sidebar() {
-  const menus = [
-    { name: "My Day", icon: "☀️" },
-    { name: "Important", icon: "⭐" },
-    { name: "Planned", icon: "📅" },
-    { name: "All", icon: "📋" },
-    { name: "Assigned to me", icon: "👤" },
-  ];
+  const lists = useAppSelector(selectAllLists);
+  const selectedListId = useAppSelector(selectSelectedListId);
+  const dispatch = useAppDispatch();
+
+  function handleListClick(listId: string) {
+    dispatch(selectList(listId));
+  }
+
+  function renderListItem(item: List) {
+    const isSelected = item.id === selectedListId;
+
+    const className = `w-full flex items-center gap-2 py-2 px-3 rounded-md transition cursor-pointer ${
+      isSelected ? "bg-neutral-800 text-white font-semibold" : "hover:bg-neutral-800 text-gray-300"
+    }`;
+
+    return (
+      <button key={item.id} onClick={() => handleListClick(item.id)} className={className}>
+        <span>{item.icon}</span>
+        <span>{item.name}</span>
+      </button>
+    );
+  }
 
   return (
-    <aside className="w-64 bg-neutral-900 text-gray-200 min-h-screen flex flex-col p-4 border-r border-neutral-700">
-      <h2 className="text-lg font-semibold mb-4 text-gray-100"> Microsoft To Do</h2>
-      <nav>
-        {menus.map((item) => (
-          <button
-            key={item.name}
-            className="w-full flex items-center gap-2 py-2 px-3 rounded-md hover:bg-neutral-800 transition"
-          >
-            <span>{item.icon}</span>
-            <span>{item.name}</span>
-          </button>
-        ))}
-      </nav>
+    <aside className="w-64 bg-neutral-900 text-gray-200 min-h-screen flex flex-col p-4 border-r border-neutral-800">
+      <h2 className="text-xl font-semibold mb-4 text-gray-100">Microsoft To Do</h2>
+      <nav>{lists.map(renderListItem)}</nav>
     </aside>
   );
 }
